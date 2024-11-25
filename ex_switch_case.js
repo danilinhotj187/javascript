ea7272f83e5644a5d15f8e63220c70e92f62b2b6
danilinhotj187 <kjdanilo187@gmail.com>
2024-11-25 20:47:14 -0300

    exercicio 1 e 2

---
+++ ex_switch_case.js
@@ -0,0 +1,66 @@
+//Exercicio 1 - Bebidas
+
+// const prompt = require("prompt-sync")()
+
+// console.log("Escolha a sua bebida:");
+// console.log("1. Água");
+// console.log("2. Suco");
+// console.log("3. Refrigerante");
+// console.log("4. Café");
+
+// const bebida = parseInt(prompt("Digite o número da sua escolha: "));
+
+// switch (bebida) {
+//   case 1:
+//     console.log("Aqui está sua Água.");
+//     break;
+//   case 2:
+//     console.log("Aqui está seu Suco.");
+//     break;
+//   case 3:
+//     console.log("Aqui está seu Refrigerante.");
+//     break;
+//   case 4:
+//     console.log("Aqui está seu Suco.");
+//     break;
+//   default:
+//     console.log("Opção inválida.");
+// }
+
+//Exercicio 2 - Operações matemáticas
+
+// const prompt = require("prompt-sync")()
+
+// console.log("\nEscolha sua operação:");
+// console.log("1. Soma");
+// console.log("2. Subtração");
+// console.log("3. Multiplicação");
+// console.log("4. Divisão");
+
+// const operacao = parseInt(prompt("Digite o número da operação: "));
+// const numero1 = parseInt(prompt("Digite o primeiro número: "));
+// const numero2 = parseInt(prompt("Digite o segundo número: "));
+
+// switch (operacao) {
+//   case 1:
+//     console.log("Resultado da soma: " + (numero1 + numero2));
+//     break;
+//   case 2:
+//     console.log("Resultado da subtração: " + (numero1 - numero2));
+//     break;
+//   case 3:
+//     console.log("Resultado da multiplicação: " + (numero1 * numero2));
+//     break;
+//   case 4:
+//     if(numero2 !== 0) {
+//     console.log("Resultado da divisão: " + (numero1 / numero2));
+//   } else {
+//     console.log("Divisão por zero não é permitida.");
+//   }
+//   break;
+// default:
+//   console.log("Opção inválida");
+//   break;
+// }
+  
+//Exercicio 3 -
