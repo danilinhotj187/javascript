a1f91e0064ed209bef09c5914f22837224b6dade
danilinhotj187 <kjdanilo187@gmail.com>
2024-11-25 21:53:38 -0300

    muitas alteracoes

--- ex_switch_case.js
+++ ex_switch_case.js
@@ -65,20 +65,20 @@
   
 //Exercicio 3 - Dcionário de cores
 
-console.log("\nTradução de Cores:");
-const cor = prompt("Digite o nome da cor em inglês (red, blue, green): ").toLowerCase();
+// console.log("\nTradução de Cores:");
+// const cor = prompt("Digite o nome da cor em inglês (red, blue, green): ").toLowerCase();
 
-switch (cor) {
-  case "red":
-    console.log("vermelho");
-    break;
-  case "blue":
-    console.log("azul");
-    break;
-  case "green":
-    console.log("verde");
-    break;
-  default:
-    console.log("Cor desconhecida");
-    break;
-}
+// switch (cor) {
+//   case "red":
+//     console.log("vermelho");
+//     break;
+//   case "blue":
+//     console.log("azul");
+//     break;
+//   case "green":
+//     console.log("verde");
+//     break;
+//   default:
+//     console.log("Cor desconhecida");
+//     break;
+// }
